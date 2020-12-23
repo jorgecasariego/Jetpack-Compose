f43124236813a3140418f53778c8e11abb95f9af
Jorge Casariego <jorge.casariego@bairesdev.com>
2020-12-23 17:51:21 -0300

    Recipe Fragment, Recipe List Fragment, Recipe Model, RecipeNetworkEntity, RecipeNetWorkMapper and EntityMappe created.

---
+++ app/src/main/java/com/jorgecasariego/mvvmrecipeapp/domain/util/EntityMapper.kt
@@ -0,0 +1,7 @@
+package com.jorgecasariego.mvvmrecipeapp.domain.util
+
+interface EntityMapper <Entity, DomainModel> {
+    fun mapFromEntity(entity: Entity): DomainModel
+
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
